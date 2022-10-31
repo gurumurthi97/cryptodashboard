c1b4d71d7ec2d06761a3f47eaf4bf74c7dc47d65
gurumurthi97 <guru13041997@gmail.com>
2022-10-31 16:05:13 +0530

    header and folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import Header from "./components/Header/index";
 function App() {
-  return <div>Hai</div>;
+  return (
+    <div>
+      Hai
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+function index() {
+  return (
+    <div className="header">
+      <p>index</p>
+    </div>
+  );
+}
+
+export default index;
