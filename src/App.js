bb965a5fba22567e8237e5484cbd88e0fbfc7578
gurumurthi97 <guru13041997@gmail.com>
2022-10-31 16:47:16 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/index";
+import Dashboard from "./pages/Dashboard";
+import Search from "./pages/Search";
 function App() {
   return (
     <div>
-      Hai
-      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
