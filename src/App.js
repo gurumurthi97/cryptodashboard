63677b94ab0328a6d68a4bf31e7868fee405264c
gurumurthi97 <guru13041997@gmail.com>
2022-11-03 13:43:13 +0530

    changed some effects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/index";
+// import Header from "./components/Header/index";
 import Dashboard from "./pages/Dashboard";
 import Search from "./pages/Search";
 import Home from "./pages/Home";
