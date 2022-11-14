64dede5bd6e82429390b8d8dd57dfb285207fb06
gurumurthi97 <guru13041997@gmail.com>
2022-11-15 00:03:13 +0530

    aaa

--- src/components/Dashboard/Linechart/index.js
+++ src/components/Dashboard/Linechart/index.js
@@ -14,7 +14,7 @@
 import React from "react";
 import "./style.css";
 import { Line } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
+import { Chart as ChartJS } from "chart.js";
 
 function LineChart({ chartData, options }) {
   return (
