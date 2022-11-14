4965dc556d4588839740e3c3928fcb058277bbce
gurumurthi97 <guru13041997@gmail.com>
2022-11-14 23:56:36 +0530

    added chartJs code

--- src/components/Dashboard/Linechart/index.js
+++ src/components/Dashboard/Linechart/index.js
@@ -1,13 +1,27 @@
+// import React from "react";
+// import "./style.css";
+// import { Line } from "react-chartjs-2";
+// import { Chart as ChartJS } from "chart.js/auto";
+// function Linechart({ chartData, options }) {
+//   return (
+//     <div>
+//       <Line data={chartData} options={options}></Line>
+//     </div>
+//   );
+// }
+
+// export default Linechart;
 import React from "react";
-import "./style.css";
+import "./styles.css";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
-function Linechart({ chartData, options }) {
+
+function LineChart({ chartData, options }) {
   return (
     <div>
-      <Line data={chartData} options={options}></Line>
+      <Line data={chartData} options={options} />
     </div>
   );
 }
 
-export default Linechart;
+export default LineChart;
